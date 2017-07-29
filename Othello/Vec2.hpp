37652a8d5326fb0a33c8cd069df2c70c57f7c6b1
Awano Yasutaka <eewano@gmail.com>
2017-07-29 11:52:26 +0900

    演算子の定義を追加する為、Vec2.hppを修正。

--- Othello/Vec2.hpp
+++ Othello/Vec2.hpp
@@ -5,51 +5,133 @@ template<typename T>
 struct Vec2
 {
 public:
+    // member
     T x, y;
     
 public:
+    /// special member functions
+    // default ctor
     Vec2()
     : x(T())
     , y(T())
     {
     }
     
+    // ctor
     constexpr Vec2(T a, T b)
     : x(a)
     , y(b)
     {
     }
     
+    // dtor
     ~Vec2() = default;
     
-    Vec2 operator*(T a)
-    {
-        return{ x * a, y * a };
-    }
     
-    Vec2 operator + (const Vec2& a)
+    
+    // plus
+    Vec2 operator+(const Vec2& a)
     {
         return{ x + a.x, y + a.y };
     }
     
+    // plus const
     const Vec2 operator+(const Vec2& a) const
     {
         return { x + a.x, y + a.y };
     }
     
-    Vec2& operator+=(const Vec2& a)
+    // minus
+    Vec2 operator-(const Vec2& a)
+    {
+        return{ x - a.x, y - a.y };
+    }
+    
+    // minus const
+    const Vec2 operator-(const Vec2& a) const
+    {
+        return{ x - a.x, y - a.y };
+    }
+    
+    // multiply single
+    Vec2 operator*(T a)
+    {
+        return{ x * a, y * a };
+    }
+    
+    // multiply vec
+    const Vec2 operator*(const Vec2& a)
+    {
+        return{ x * a.x, y * a.y };
+    }
+    
+    // divide
+    Vec2 operator/(T a)
+    {
+        return{ x / a, y / a };
+    }
+    
+    // divide multiple
+    const Vec2 operator/(const Vec2& a)
+    {
+        return{ x / a.x, y / a.y };
+    }
+    
+    // plus equals
+    Vec2& operator+=(Vec2 a)
     {
         x += a.x;
         y += a.y;
         return *this;
     }
     
-    const Vec2& operator+=(const Vec2& a) const
+    // minus equals
+    Vec2& operator-=(Vec2 a)
     {
-        x += a.x;
-        y += a.y;
+        x -= a.x;
+        y -= a.y;
         return *this;
     }
+    
+    // assign
+    Vec2& operator=(const Vec2& a)
+    {
+        x = a.x;
+        y = a.y;
+        return *this;
+    }
+    
+    // compare
+    bool operator<(const Vec2& a) const
+    {
+        return x < a.x && y < a.y;
+    }
+    
+    bool operator<=(const Vec2& a) const
+    {
+        return x <= a.x && y <= a.y;
+    }
+    
+    bool operator>(const Vec2& a) const
+    {
+        return !(*this <= a);
+    }
+    
+    bool operator>=(const Vec2& a) const
+    {
+        return !(*this < a);
+    }
+    
+    bool operator==(const Vec2& a) const
+    {
+        return x == a.x && y == a.y;
+    }
+    
+    bool operator!=(const Vec2& a) const
+    {
+        return !(*this == a);
+    }
+    
 };
 
 using Vec2f = Vec2<float>;
