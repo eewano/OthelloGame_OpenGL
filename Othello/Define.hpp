582ba240045a4e729e925aea3890e0220a6d742d
Awano Yasutaka <eewano@gmail.com>
2017-07-30 14:03:37 +0900

    定数の一部をGameManager.hppに移行。

--- Othello/Define.hpp
+++ Othello/Define.hpp
@@ -6,10 +6,6 @@
 //現時点では WINDOW_SIZE が 800,600 でないとカーソル位置の取得がおかしくなり、正常にゲームが遊べない
 static Vec2i WINDOW_SIZE = { 800, 600 };
 static float ASPECT_RATIO = static_cast<float>(WINDOW_SIZE.x) / WINDOW_SIZE.y;
-static const int BOARD_SIZE = 8;
-static const float BOARD_POS_X = -0.35f;
-static const float BOARD_POS_Y = -0.35f;
-static constexpr int VERTS_COUNT = 4;
 
 #undef min
 #undef max
